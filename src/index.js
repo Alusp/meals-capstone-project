4529820df0a351192c194224b9a4c8e8fc67a905
skyv26 <dev.aakashv@gmail.com>
2022-08-31 16:45:47 +0530

    Get the `item-id`

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import "./style.css"
-alert("Hello")
+import Api from "../modules/Api";
+
+const getData = Api;
+const { meals } = getData.get();
+meals.forEach((each) => {
+  console.log(each.idMeal);
+})
