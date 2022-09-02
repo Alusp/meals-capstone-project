5bdf92f01ac6ece4a77d75ab9fb689532392ac59
skyv26 <dev.aakashv@gmail.com>
2022-09-02 17:34:23 +0530

    Add header `JavaScript` functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import './style.css';
 import Main from '../components/Main/Main.js';
 
+const menuParent = document.querySelector('.nav-menu_ul');
+
+const allMenu = document.querySelectorAll('.nav-menu_option');
 const root = document.querySelector('#root');
 root.append(Main());
+
+menuParent.addEventListener('click', (e) => {
+  allMenu.forEach((each) => each.classList.remove('active'));
+  e.target.classList.add('active');
+  const getNode = e.target;
+  if (getNode.textContent === 'Most Liked') {
+    const allMenuItem = document.querySelectorAll('.meals-grid_wrapper-list');
+    allMenuItem.forEach((each) => {
+      const counter = each.querySelector('.counter');
+      if (counter.textContent === '(0)') {
+        each.remove();
+      }
+    });
+  } else if (getNode.textContent.includes('Meals')) {
+    root.append(Main());
+  }
+});
